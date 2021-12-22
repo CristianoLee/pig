85add89ca33d7019d94e89d7091e46dfdc3ab30d
CristianoLee <85106702+CristianoLee@users.noreply.github.com>
2021-12-23 04:14:06 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,16 +9,43 @@ var app = new Vue({
             'images/rou5.png',
             'images/rou6.png',
         ],
-        index: 0
+        index: 0,
+        timer: null,
+        type: '暂停'
     },
     mounted() {
-        var that = this;
-        setInterval(function () {
-            that.next();
-        }, 100);
+        this.test(100);
     },
     methods: {
-
+        normol() {
+            this.test(1000);
+        },
+        fast() {
+            this.test(100);
+        },
+        slow() {
+            this.test(3000);
+        },
+        stop() {
+            if (this.type == '暂停') {
+                this.type = '继续';
+                clearInterval(this.timer);
+            } else {
+                this.type = '暂停';
+                let that = this;
+                that.timer = setInterval(function () {
+                    that.next();
+                }, 100);
+            }
+        },
+        test(time) {
+            let that = this;
+            clearInterval(that.timer);
+            that.type = '暂停';
+            that.timer = setInterval(function () {
+                that.next();
+            }, time);
+        },
         prev() {
             if (this.index == 0) {
                 this.index = 5;
@@ -32,6 +59,6 @@ var app = new Vue({
             } else {
                 this.index++;
             }
-        },
+        }
     }
 })
